7b7a4b876a6533c193c25a32ef452d06e20ac81d
Charles Morin <charles.mor@outlook.fr>
2026-04-10 11:54:26 -0400

    Fixed issue with babylon gui-editor dep

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,9 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig({});
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@babylonjs/gui-editor/guiEditor.js': '@babylonjs/gui-editor',
+    },
+  },
+});
